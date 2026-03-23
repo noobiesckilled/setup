0d8d1622f814f88bf1027e9ee823eff3283f6b11
JaswanthSai <142310702+noobiesckilled@users.noreply.github.com>
2026-03-23 16:19:09 +0530

    Add Jenkins installation script for Amazon Linux

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+sudo yum update –y
+sudo yum install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install jenkins git -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
